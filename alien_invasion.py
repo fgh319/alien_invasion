8525f3a1f121ec878ebf3ea293a89952f3dc83fb
fgh319 <fengguihang@outlook.com>
2025-08-30 22:08:49 +0800

    在全屏模式下运行游戏

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,6 +12,11 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
+        # 在全屏模式下运行游戏
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
